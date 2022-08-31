af4f065e8c287c9673267f21352a0448f283877d
Tristan Dobrowolski <dt39252@zut.edu.pl>
2022-08-31 11:41:47 +0200

    Remove characters not supported by the LCD

--- host/examples/datetime_display/datetime_display.sh
+++ host/examples/datetime_display/datetime_display.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CURRENT_DATETIME=$(date)
+CURRENT_DATETIME=$(date | sed 'y/ęóąśćłżź/eoasclzz/') # replace special polish characters
 mmlcdctl p "${CURRENT_DATETIME:0:16}"
 mmlcdctl p " ${CURRENT_DATETIME:18:32}" 2
